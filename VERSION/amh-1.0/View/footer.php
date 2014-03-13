e06bc253aae48cbd1d2f4adb68159b452fa1a922
xiqingongzi <xiqingongzi@gmail.com>
2014-03-13 22:49:50 +0800

    Del Litter

--- VERSION/amh-1.0/View/footer.php
+++
@@ -1,6 +0,0 @@
-
-<div id="footer" >
-AMH1.0 [lnmp] Powered by Amysql.com
-</div>
-</body>
-</html>
--- VERSION/amh-2.0/View/footer.php
+++
@@ -1,6 +0,0 @@
-
-<div id="footer" >
-AMH2.0 [lnmp] Powered by Amysql.com
-</div>
-</body>
-</html>
